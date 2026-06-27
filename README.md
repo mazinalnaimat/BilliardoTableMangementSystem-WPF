3ffcdbc56bcafa39a64393fc4d02f774a2c3c197
Mazin Al-Naimat <mazinalnaimat@gmail.com>
2026-06-27 16:22:49 +0300

    V1 BTMS-WPF

---
+++ README.md
@@ -0,0 +1,8 @@
+Simple Program with WPF-C# to manage billiardo tables by calculate price per time for each table
+
+<img src="imgs/BTMS.png" width="800">
+
+<br>
+<t3>Program Demo:</h3>
+
+<img src="imgs/BTMS.gif" width="800">
---
+++ RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace BilliardGameTablesManagement
+{
+    // A simple reusable RelayCommand (you can place it in a separate file)
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool>?_canExecute;
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke() ?? true;
+        public void Execute(object parameter) => _execute();
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
