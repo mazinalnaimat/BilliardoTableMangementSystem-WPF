43089c80dfb236d43012906b910d32bacd079360
Mazin Al-Naimat <mazinalnaimat@gmail.com>
2026-07-02 11:22:28 +0300

    add current time to BilliardTableCardView

--- BilliardGameTablesManagement.UI.WFP/ViewModels/Windows/BilliardTablesViewModel.cs
+++ BilliardGameTablesManagement.UI.WFP/ViewModels/Windows/BilliardTablesViewModel.cs
@@ -2,13 +2,29 @@ using BilliardGameTablesManagement.Business.DTOs;
 using BilliardGameTablesManagement.Business.Interfaces;
 using BilliardGameTablesManagement.Services.Implementations;
 using BilliardGameTablesManagement.ViewModels.Controls;
+using System;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Windows.Threading;
 
 namespace BilliardGameTablesManagement.ViewModels.Windows
 {
     public class BilliardTablesViewModel : BaseViewModel
     {
         private readonly ITableSessionService _tableSessionService;
+        private readonly DispatcherTimer _clockTimer;
+
+        private string _currentTime;
+
+        public string CurrentTime
+        {
+            get => _currentTime;
+            set
+            {
+                _currentTime = value;
+                OnPropertyChanged();
+            }
+        }
 
         public ObservableCollection<BilliardTableCardViewModel> Tables { get; }
 
@@ -20,6 +36,18 @@ namespace BilliardGameTablesManagement.ViewModels.Windows
             Tables = new ObservableCollection<BilliardTableCardViewModel>();
 
             LoadTables();
+
+            CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+
+            _clockTimer = new DispatcherTimer();
+            _clockTimer.Interval = TimeSpan.FromSeconds(1);
+            _clockTimer.Tick += ClockTimer_Tick;
+            _clockTimer.Start();
+        }
+
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            CurrentTime = DateTime.Now.ToString("HH:mm:ss");
         }
 
         private void LoadTables()
